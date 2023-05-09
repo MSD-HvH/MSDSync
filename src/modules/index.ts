c5b7a8d1543b8f1483793dac8f60f5b201d1f2df
MasedMSD <roblox.hack@mail.ru>
2023-05-09 13:15:02 +0500

    clear project

--- src/modules/index.ts
+++
@@ -1,7 +0,0 @@
-export * from "./Color.js";
-export * from "./Drag.js";
-export * from "./Easings.js";
-export * from "./Input.js";
-export * from "./TimeFormat.js";
-export * from "./Utils.js";
-export * from "./Window.js";
