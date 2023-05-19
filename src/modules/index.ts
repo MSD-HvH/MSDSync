addb352eb4edadb33da6b09d6bdac3b7cbb7aa48
MasedMSD <roblox.hack@mail.ru>
2023-05-19 18:54:29 +0500

    Easings

--- src/modules/index.ts
+++ src/modules/index.ts
@@ -2,6 +2,7 @@ export * from "./onetap-ui/index.js";
 
 export * from "./Color.js";
 export * from "./Drag.js";
+export * from "./Easings.js";
 export * from "./InputSystem.js";
 export * from "./TimeFormat.js";
 export * from "./Utils.js";
