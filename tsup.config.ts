ad46af409cd1e6dcbd98152c7d793d67582c7dc3
MasedMSD <roblox.hack@mail.ru>
2023-07-17 22:16:19 +0500

    fuck rollup, go tsup

--- tsup.config.ts
+++ tsup.config.ts
@@ -3,12 +3,10 @@ import { defineConfig } from "tsup";
 export default defineConfig({
 	entry: ["src/index.ts"],
 	target: "es5",
-	// format: ["cjs"],
-	// sourcemap: true,
-	// clean: true,
-	// dts: true,
-	// bundle: false,
-	// tsconfig: "./tsconfig.json",
-	// config: false,
+	format: ["cjs", "esm"],
+	sourcemap: true,
+	clean: true,
+	dts: true,
+	bundle: false,
 	outDir: "C:/Games/Steam/steamapps/common/2020/ot/scripts",
 });
