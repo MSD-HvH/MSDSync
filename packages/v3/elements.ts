29d2f57906ceebd215d1b385bf5df99cdf8adcbf
MasedMSD <roblox.hack@mail.ru>
2023-08-01 23:35:09 +0500

    test for all MSDSync elements

---
+++ packages/v3/elements.ts
@@ -0,0 +1,71 @@
+(function CreateErrorHandler(fnCallback) {
+	Duktape.errCreate = function (e) {
+		if (!(e instanceof Error) || "thrown" in e || !Object.isExtensible(e)) return e;
+		e = fnCallback(e);
+		return e;
+	};
+})(function (e: any) {
+	e.time = new Date();
+	Cheat.Print("Found error in the script code, please send next message to the developer: \n");
+	Cheat.PrintColor([44, 3, 252, 255], "Information for the developer: error at line " + e.lineNumber + "\n\n");
+	return e;
+});
+
+const elementY = 100;
+
+import {
+	InputSystem,
+	MSDSyncCheckbox,
+	MSDSyncDropdown,
+	MSDSyncMultiDropdown,
+	MSDSyncSlider,
+	Window,
+	Callbacks,
+} from "./src/index.js";
+
+const input = new InputSystem();
+const checkbox = new MSDSyncCheckbox({ name: "MSDSync Checkbox", window: new Window({ x: 100, y: elementY }) });
+const dropdown = new MSDSyncDropdown({
+	name: "MSDSync Dropdown",
+	elements: ["Hello", "World"],
+	window: new Window({ x: 100, y: elementY + 80, width: 140 }),
+});
+const multiDropdown = new MSDSyncMultiDropdown({
+	name: "MSDSync MultiDropdown",
+	elements: ["Hello", "World"],
+	window: new Window({ x: 100, y: elementY + 80 * 2, width: 140 }),
+});
+const slider = new MSDSyncSlider({
+	name: "MSDSync Slider",
+	min: 0,
+	max: 100,
+	window: new Window({ x: 100, y: elementY + 80 * 3, width: 140 }),
+});
+const CallbackRegister = new Callbacks();
+
+CallbackRegister.on("Draw", () => {
+	input.UpdateInputSystem();
+
+	const font = Render.AddFont("Segoeui.ttf", 8, 200);
+
+	checkbox.RenderBox();
+	checkbox.RenderText({ font });
+
+	dropdown.RenderBox();
+	dropdown.RenderElements({ font });
+	dropdown.RenderText({ font });
+
+	multiDropdown.RenderBox();
+	multiDropdown.RenderElements({ font });
+	multiDropdown.RenderText({ font });
+
+	slider.RenderBox();
+	slider.RenderText({ font });
+
+	checkbox.HandleClick({ input });
+	dropdown.HandleClick({ input });
+	multiDropdown.HandleClick({ input });
+	slider.HandleClick({ input });
+});
+
+CallbackRegister.RegisterCallbacks();
--- packages/v3/index.ts
+++ packages/v3/index.ts
@@ -1,69 +1,9 @@
-(function CreateErrorHandler(fnCallback) {
-	Duktape.errCreate = function (e) {
-		if (!(e instanceof Error) || "thrown" in e || !Object.isExtensible(e)) return e;
-		e = fnCallback(e);
-		return e;
-	};
-})(function (e: any) {
-	e.time = new Date();
-	Cheat.Print("Found error in the script code, please send next message to the developer: \n");
-	Cheat.PrintColor([44, 3, 252, 255], "Information for the developer: error at line " + e.lineNumber + "\n\n");
-	return e;
-});
+import { Callbacks } from "./src/index.js";
 
-import {
-	InputSystem,
-	MSDSyncCheckbox,
-	MSDSyncDropdown,
-	MSDSyncMultiDropdown,
-	MSDSyncSlider,
-	Window,
-	Callbacks,
-} from "./src/index.js";
-
-const input = new InputSystem();
-const checkbox = new MSDSyncCheckbox({ name: "MSDSync Checkbox", window: new Window({ x: 100, y: 100 }) });
-const dropdown = new MSDSyncDropdown({
-	name: "MSDSync Dropdown",
-	elements: ["Hello", "World"],
-	window: new Window({ x: 100, y: 200, width: 140 }),
-});
-const multiDropdown = new MSDSyncMultiDropdown({
-	name: "MSDSync MultiDropdown",
-	elements: ["Hello", "World"],
-	window: new Window({ x: 100, y: 300, width: 140 }),
-});
-const slider = new MSDSyncSlider({
-	name: "MSDSync Slider",
-	min: 0,
-	max: 100,
-	window: new Window({ x: 100, y: 400, width: 140 }),
-});
 const CallbackRegister = new Callbacks();
 
 CallbackRegister.on("Draw", () => {
-	input.UpdateInputSystem();
-
-	const font = Render.AddFont("Segoeui.ttf", 8, 200);
-
-	checkbox.RenderBox();
-	checkbox.RenderText({ font });
-
-	dropdown.RenderBox();
-	dropdown.RenderElements({ font });
-	dropdown.RenderText({ font });
-
-	multiDropdown.RenderBox();
-	multiDropdown.RenderElements({ font });
-	multiDropdown.RenderText({ font });
-
-	slider.RenderBox();
-	slider.RenderText({ font });
-
-	checkbox.HandleClick({ input });
-	dropdown.HandleClick({ input });
-	multiDropdown.HandleClick({ input });
-	slider.HandleClick({ input });
+	Cheat.Print("Hello world \n");
 });
 
 CallbackRegister.RegisterCallbacks();
